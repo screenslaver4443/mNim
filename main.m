bb239447a88126f3fa90dea3a6ddde7d8a52b194
Nikolai Pesudovs <npesudovs@outlook.com>
2025-03-31 13:51:42 +1030

    Updated Display to look nicer

--- main.m
+++ main.m
@@ -2,18 +2,34 @@
 
 % Initialize variables
 state = "game"; % game, title
-board = [
-    " ", " ", " ", "I", " ", " ", " ";
-    " ", " ", "I", "I", "I", " ", " ";
-    " ", "I", "I", "I", "I", "I", " ";
-    "I", "I", "I", "I", "I", "I", "I";
-    ];
+playing = true;
 
 % Switch statement to differentiate between title screen and game
-switch state
-    case "title"
-        % Placeholder
+while playing
+    switch state
+        case "title"
+            % Placeholder
 
-    case "game"
-        disp(board);
+        case "game"
+            board = [
+                0, 0, 0, 1, 0, 0, 0;
+                0, 0, 1, 1, 1, 0, 0;
+                0, 1, 1, 1, 1, 1, 0;
+                1, 1, 1, 1, 1, 1, 1;
+                ];
+            % For loop
+            for r = 1:height(board)
+                for c = 1:length(board)
+                    if board(r, c) == 1
+                        fprintf('I');
+                    else
+                        fprintf(' ');
+                    end
+                end
+                fprintf('\n');
+            end
+            break %Temporary break to prevent infinite loop
+
+            % disp(board);
+    end
 end
