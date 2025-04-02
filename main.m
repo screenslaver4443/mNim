2f0bfcde9a9541b3d246e9eaf5820107b453ab61
Nikolai Pesudovs <npesudovs@outlook.com>
2025-04-02 14:49:31 +1030

    Counts how many matches are left

--- main.m
+++ main.m
@@ -1,11 +1,11 @@
-% This file contains the game NIM. (https://en.wikipedia.org/wiki/Nim)
 
 % Initialize variables
 state = "game"; % game, title
-playing = true;
+% This file contains the game NIM. (https://en.wikipedia.org/wiki/Nim)
+running = true;
 
 % Switch statement to differentiate between title screen and game
-while playing
+while running
     switch state
         case "title"
             % Placeholder
@@ -17,19 +17,28 @@ while playing
                 0, 1, 1, 1, 1, 1, 0;
                 1, 1, 1, 1, 1, 1, 1;
                 ];
-            % For loop
-            for r = 1:height(board)
-                for c = 1:length(board)
-                    if board(r, c) == 1
-                        fprintf('I');
-                    else
-                        fprintf(' ');
+            playing = true;
+            while playing
+                matchcount = 0;
+                % For loop
+                for r = 1:height(board)
+                    for c = 1:length(board)
+                        if board(r, c) == 1
+                            fprintf('I');
+                            matchcount = matchcount+1;
+                        else
+                            fprintf(' ');
+                        end
                     end
+                    if r == 1
+                        row1MatchCount = matchcount;
+                    end
+                    fprintf('\n');
                 end
-                fprintf('\n');
-            end
-            break %Temporary break to prevent infinite loop
+                fprintf('\n There is %d left in row & %d matches left in total \n', row1MatchCount, matchcount)
+                input(sprintf('How many matches do you remove (up to %d)? ', row1MatchCount));
 
-            % disp(board);
+            end
     end
+    % disp(board);
 end
